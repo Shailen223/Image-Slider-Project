1207a1eb36f662aa829ace50756a5b03ffd481e1
Shailen Nanu <s.nanu987@gmail.com>
2023-06-22 10:44:25 +0800

    another one

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const images = document.querySelectorAll('.images');
 const slider = document.querySelector('.slider');
-console.log(images);
-let current = 1;
+console.log(images)
+let current = 1
 const slideWidth = 800;
 
 const firstslide = () => {
-  slider.style.transform = 'translate(0)';
+  slider.style.transform = 'translate(0)'
   current = 1;
 }
 
-const lastslide = () => {
-  slider.style.transform = `translate(-${(images.length - 1) * slideWidth}px)`;
+const lastlide = () => {
+  slider.style.transform = `translateX(-${(images.length - 1) * slideWidth}px)`
   current = images.length;
 }
 
 const prevslide = () => {
-  slider.style.transform = `translate(-${(current - 2) * slideWidth}px)`;
+  slider.style.transform = `translateX(-${(current - 2) * slideWidth}px)`
     current--;
 }
 
 const nextslide = () => {
-  slider.style.transform = `translate(-${current * slideWidth}px)`;
+  slider.style.transform = `translateX(-${current * slideWidth}px)`
     current++;
 }
 
 left.addEventListener('click', () => {
-  current > 1? prevslide() : lastslide();
-})
+  current > 1? prevslide() : lastlide();
+});
 
 right.addEventListener('click', () => {
   current < images.length? nextslide() : firstslide();
-})
+});
