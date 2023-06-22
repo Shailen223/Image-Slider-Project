156b132bb8c14375433d27fbeb6d45cdf41f3deb
Shailen Nanu <s.nanu987@gmail.com>
2023-06-22 10:01:01 +0800

    Recreated javascript part

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-//Use ChatGPT to help explain the code
-
-const right = document.querySelector('.right');
 const left = document.querySelector('.left');
+const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.images');
 console.log(images);
 const slideWidth = 800;
 let current = 1;
 
-const nextslide = () => {
-  slider.style.transform = `translateX(-${current * slideWidth}px)`;
-    current++;
-}
-
 const firstslide = () => {
-  slider.style.transform = 'translateX(0)';
+  slider.style.transform = 'translate(0)';
   current = 1;
 }
 
+const lastslide = () => {
+  slider.style.transform = `translate(-${(images.length - 1) * 800}px)`;
+  current = images.length;
+}
+
 const prevslide = () => {
-  slider.style.transform = `translateX(-${(current - 2)*800}px)`;
+  slider.style.transform = `translate(-${(current - 2) * 800}px)`;
     current--;
 }
 
-const lastslide = () => {
-  slider.style.transform = `translateX(-${(images.length - 1) * 800}px)`;
-    current = images.length;
+const nextslide = () => {
+  slider.style.transform = `translate(-${current * slideWidth}px)`;
+    current++;
 }
 
 left.addEventListener('click', () => {
-  // another way of if/else
   current > 1? prevslide() : lastslide();
 });
 
-
-
 right.addEventListener('click', () => {
-  // another way of if/else
   current < images.length? nextslide() : firstslide();
-});
+})
 
-console.log(left,"left", right); 
-
-
+console.log(left,"left",right)
